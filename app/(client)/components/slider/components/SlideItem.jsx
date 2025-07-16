59348d620f2a12014464b367374cfcaf7fa2641a
Diego <diego_torres_11@hotmail.com>
2025-07-16 13:42:56 -0500

    probando cambios en resolucion

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -12,15 +12,17 @@ export const SlideItem = ({ slides, current }) => {
     key={index}
     className="absolute inset-0 opacity-100"
   >
-    <Image
-      src={slide.imgSrc}
-      alt={slide.altText}
-      fill
-      className="object-cover object-center brightness-75"
-      priority
-      fetchPriority="high"
-      sizes="100vw"
-    />
+   <div className="relative w-full h-[70vh]">
+  <Image
+    src={slide.imgSrc}
+    alt={slide.altText}
+    width={1920}
+    height={1080}
+    className="w-full h-full object-cover object-center brightness-75"
+    priority
+    fetchPriority="high"
+  />
+</div>
 
       {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
