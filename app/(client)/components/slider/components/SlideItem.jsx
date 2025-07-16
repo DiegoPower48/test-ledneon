b5b3ead4847eb75854a52b802c1db991c71e2ed7
Diego <diego_torres_11@hotmail.com>
2025-07-16 13:37:34 -0500

    probando eliminando transicion en slider

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -9,18 +9,18 @@ export const SlideItem = ({ slides, current }) => {
   {slides.map((slide, index) =>
   index === current ? (
     <div
-      key={index}
-      className="absolute inset-0 transition-opacity duration-500 opacity-100"
-    >
-      <Image
-        src={slide.imgSrc}
-        alt={slide.altText}
-        fill
-        className="object-cover object-center brightness-75"
-        priority
-        fetchPriority="high"
-        sizes="100vw"
-      />
+    key={index}
+    className="absolute inset-0 opacity-100"
+  >
+    <Image
+      src={slide.imgSrc}
+      alt={slide.altText}
+      fill
+      className="object-cover object-center brightness-75"
+      priority
+      fetchPriority="high"
+      sizes="100vw"
+    />
 
       {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
