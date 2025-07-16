90c1eba3bfce22bed30e0acaff4ba8f9dba3ebb8
Diego <diego_torres_11@hotmail.com>
2025-07-16 13:27:03 -0500

    arreglando fetch priority

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -6,20 +6,20 @@ import React from "react";
 export const SlideItem = ({ slides, current }) => {
   return (
  <>
-  {slides.map((slide, index) => (
+  {slides.map((slide, index) =>
+  index === current ? (
     <div
       key={index}
-      className={`absolute inset-0 transition-opacity duration-500 ${
-        index === current ? "opacity-100" : "opacity-0"
-      }`}
+      className="absolute inset-0 transition-opacity duration-500 opacity-100"
     >
-      {/* Imagen */}
       <Image
         src={slide.imgSrc}
         alt={slide.altText}
         fill
         className="object-cover object-center brightness-75"
         priority
+        fetchPriority="high"
+        sizes="100vw"
       />
 
       {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
@@ -27,8 +27,9 @@ export const SlideItem = ({ slides, current }) => {
 
       {/* Borde derecho borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute right-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
-    </div>
-  ))}
+     </div>
+  ) : null
+)}
 </>
 
 
