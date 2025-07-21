7cd2188a443f58788c21f9eb050bf769e80da7ce
Diego <diego_torres_11@hotmail.com>
2025-07-21 13:30:54 -0500

    prioridad

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export const SlideItem = ({ slides, current }) => {
   const [isMobile, setIsMobile] = useState(false);
 
-  const slide = slides[current]
+  const slide = slides[current];
 
   useEffect(() => {
     const checkMobile = () => setIsMobile(window.innerWidth < 768);
@@ -14,27 +14,24 @@ export const SlideItem = ({ slides, current }) => {
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
-  const imageSrc = isMobile ? (slide.imgSrcMobile || slide.imgSrc) : slide.imgSrc;
+  const imageSrc = isMobile ? slide.imgSrcMobile || slide.imgSrc : slide.imgSrc;
 
   return (
-  <div className="relative w-full h-[500px] sm:h-[700px] overflow-hidden">
-  <img
-    src={imageSrc}
-    alt={slide.altText}
-    loading="lazy"
-    decoding="async"
-    fetchPriority="high"
-    className="object-cover object-center w-full h-full brightness-75"
-  />
+    <div className="relative w-full h-[500px] sm:h-[700px] overflow-hidden">
+      <img
+        src={imageSrc}
+        alt={slide.altText}
+        decoding="async"
+        fetchPriority="high"
+        className="object-cover object-center w-full h-full brightness-75"
+      />
 
-          {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
-            <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
+      {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
+      <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
 
-            {/* Borde derecho borroso - visible solo en pantallas md+ */}
-            <div className="hidden md:block absolute right-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
-        
-</div>
-
+      {/* Borde derecho borroso - visible solo en pantallas md+ */}
+      <div className="hidden md:block absolute right-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
+    </div>
   );
 };
 
