8fe0f23f080c68e8c478faf7d0fa63aa635ce16f
Diego <diego_torres_11@hotmail.com>
2025-07-22 13:19:13 -0500

    probando el preload

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 
-export const SlideItem = ({ slides, current,slidesMovil }) => {
+export const SlideItem = ({ slides, current, slidesMovil }) => {
   const [isMobile, setIsMobile] = useState(false);
-
 
   useEffect(() => {
     const checkMobile = () => setIsMobile(window.innerWidth < 768);
@@ -11,38 +10,26 @@ export const SlideItem = ({ slides, current,slidesMovil }) => {
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
-  const generateCloudinaryUrl = () => {
-      if(isMobile){
-return slidesMovil[current].imgSrc
-      }
-      return slides[current].imgSrc
-
-
-    // const base = "upload/";
-    // const insert = isMobile
-    //   ? "w_768,h_450,c_fill,f_auto,q_auto"
-    //   : "w_1440,h_700,c_fill,f_auto,q_auto";
-
-    // return url.replace(base, `${base}${insert}/`);
-  };
-
-  // const imageSrc = generateCloudinaryUrl(slide.imgSrc);
+  const slide = isMobile ? slidesMovil[current] : slides[current];
 
   return (
     <div className="relative w-full h-[500px] sm:h-[700px] overflow-hidden">
       <img
-        src={generateCloudinaryUrl()}
-        alt={slides[current].altText}
-         width={128}
-  height={160}
-        decoding="async"
-        fetchPriority="high"
-        className="object-cover object-center w-full h-full brightness-75"
-      />
+  src={slide.imgSrc}
+  srcSet={`${slide.imgSrc.replace(".webp", "")}_mobile.webp 768w, ${slide.imgSrc} 1440w`}
+  sizes="(max-width: 768px) 100vw, 1440px"
+  alt={slide.altText}
+  width={isMobile ? 768 : 1440}
+  height={isMobile ? 450 : 700}
+  className="object-cover object-center w-full h-full brightness-75"
+  decoding="async"
+  fetchPriority="high"
+  loading="eager"
+/>
+
 
       {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
-
       {/* Borde derecho borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute right-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
     </div>
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Montserrat } from 'next/font/google';
 import "../styles/globals.css";
 import { WhatsAppButton } from "./(client)/components/index";
+import Head from 'next/head';
 
 const montserrat = Montserrat({
     subsets: ['latin'],
@@ -17,6 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head  >
+<link rel="preload" as="image" href="https://res.cloudinary.com/dqaetlpog/image/upload/v1752942676/logo_mlg_letras_doradas_con_iluminacion_s2wy9w.webp" />
+        <link rel="preload" as="image" href="/home/logo_mlg_letras_doradas_con_iluminacion_mobile.webp" />
+      </head>
       <body
         className={`${montserrat.variable} antialiased`}
       >
