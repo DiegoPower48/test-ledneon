dfdc11d0c5f96664a5f5997ea80482f430b68b17
Diego <diego_torres_11@hotmail.com>
2025-07-24 10:21:39 -0500

    mas pruebas

--- app/(client)/components/footer/Footer.jsx
+++ app/(client)/components/footer/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
         <>
             <footer className="overflow-hidden h-[900px] lg:h-auto bg-gradient-to-r from-[--azul_brillante] to-[--azul_intenso] w-full pb-20">
                 <div className="after:w-[200%] h-[800px] lg:h-auto after:translate-x-[-25%] after:z-10 after:absolute after:inset-0 relative after:bg-[--azul_oscuro] pt-16 pb-72  after:rounded-b-[50%]">
-                    <div className="rounded-full flex justify-center items-center p-7 bg-white overflow-hidden absolute w-[130px]  right-[calc(50%-65px)] bottom-[0] translate-y-[40%] z-30">
+                    {/* <div className="rounded-full flex justify-center items-center p-7 bg-white overflow-hidden absolute w-[130px]  right-[calc(50%-65px)] bottom-[0] translate-y-[40%] z-30">
                         <img className="w-[80px]" src="/header_footer/logo_azul_letraNegra_ledneonpublicidad.webp" alt="Logotipo de Neon LED Publicidad con letras negras" />
-                    </div>
+                    </div> */}
                     <div className="footer-content absolute pt-10 inset-0 z-20 flex flex-col justify-center lg:flex-row lg:justify-center mx-28 gap-12 text-center lg:text-left text-white font-semibold mb-28">
                         <ul>
                             <li className="flex flex-col">
@@ -48,10 +48,10 @@ export default function Footer() {
                         <div className="flex flex-col items-center lg:items-start">
                             <h2 className="text-[--azul_brillante] text-lg lg:text-xl">Reclamaciones</h2>
                             <span className="w-[30px] mx-auto lg:mx-0 h-[6px] border-t-2 border-solid border-t-[--azul_brillante]"></span>
-                            <Link className="w-[200px] m-1" href={"/reclamaciones"}>
+                            {/* <Link className="w-[200px] m-1" href={"/reclamaciones"}>
                                 <p className="text-center">Libro de reclamos</p>
                                 <img className="w-full" src="/reclamaciones/librodereclamaciones_ledneonpublicidad.webp" alt="Ilustración de un libro de reclamaciones abierto con páginas blancas" />
-                            </Link>
+                            </Link> */}
                         </div>
                     </div>
                 </div>
