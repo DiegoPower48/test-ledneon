cf197266b4bdbc34cf28feb74950377275ef0113
Diego <diego_torres_11@hotmail.com>
2025-07-30 13:06:17 -0500

    probando chatgpt

--- app/(client)/productos/components/ProductoIndividual.jsx
+++ app/(client)/productos/components/ProductoIndividual.jsx
@@ -6,8 +6,8 @@ function Producto({ imgSrc, altText, description, route, imgSrcMobile }) {
     <a href={route} className={styles["producto-link"]}>
       <div className={styles.producto}>
         <img src={imgSrc} alt={altText} className={styles["producto-img"]}
-        srcSet={`${imgSrc} 800w, ${imgSrcMobile} 400w`}
-        
+      srcSet={`${imgSrcMobile} 200w, ${imgSrc} 800w`}
+  sizes="(max-width: 768px) 200px, 800px"
         />
         <div className={styles["producto-description"]}>
           <h3 className={`${styles["producto-description__text"]} text-xs md:text-base`}>{description}</h3>
