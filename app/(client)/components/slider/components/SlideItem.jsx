ab438d7574bef7f4ae102697804b585e945fd1e4
Diego <diego_torres_11@hotmail.com>
2025-07-30 20:14:59 -0500

    probando

--- app/(client)/components/slider/components/SlideItem.jsx
+++ app/(client)/components/slider/components/SlideItem.jsx
@@ -4,27 +4,20 @@ import Image from "next/image";
 import React from "react";
 
 export const SlideItem = ({ slides, current }) => {
+  const slide = slides[current];
+
   return (
- <>
-  {slides.map((slide, index) => (
-    <div
-      key={index}
-      className={`absolute inset-0 transition-opacity duration-500 ${
-        index === current ? "opacity-100" : "opacity-0"
-      }`}
-    >
-      {/* Imagen */}
-    <Image
-  src={slide.imgSrc}
-  alt={slide.altText}
-  width={400} // o el ancho real en móvil
-  height={225}
-  priority
-  fetchPriority="high"
-  decoding="async"
-  className="object-cover object-center"
-  sizes="(max-width: 767px) 100vw, (max-width: 1023px) 80vw, 60vw"
-/>
+    <div className="absolute inset-0">
+      <Image
+        src={slide.imgSrc}
+        alt={slide.altText}
+        fill
+        priority
+        fetchPriority="high"
+        decoding="async"
+        className="object-cover object-center"
+        sizes="(max-width: 767px) 100vw, (max-width: 1023px) 80vw, 60vw"
+      />
 
       {/* Borde izquierdo borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute left-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
@@ -32,9 +25,5 @@ export const SlideItem = ({ slides, current }) => {
       {/* Borde derecho borroso - visible solo en pantallas md+ */}
       <div className="hidden md:block absolute right-0 top-0 h-full w-16 bg-black/10 backdrop-blur-sm pointer-events-none z-10" />
     </div>
-  ))}
-</>
-
-
   );
 };
